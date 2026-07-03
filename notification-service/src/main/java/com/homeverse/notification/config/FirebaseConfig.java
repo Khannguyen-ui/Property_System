4779696e54292097e45b855c0f0ba68edcee1c21
ThienAn242005 <levothienan242005@gmail.com>
2026-07-03 22:59:58 +0700

    Add FCM notification backend

--- notification-service/src/main/java/com/homeverse/notification/config/FirebaseConfig.java
+++ notification-service/src/main/java/com/homeverse/notification/config/FirebaseConfig.java
@@ -1,36 +1,35 @@
 package com.homeverse.notification.config;
 
+import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.FileInputStream;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.config.path}")
+    private String firebasePath;
+
     @PostConstruct
-    public void init() {
-        try {
-            if (!FirebaseApp.getApps().isEmpty()) return;
+    public void init() throws Exception {
 
-            InputStream serviceAccount = getClass()
-                    .getClassLoader()
-                    .getResourceAsStream("firebase/firebase-service-account.json");
-
-            if (serviceAccount == null) {
-                throw new IllegalStateException("Firebase service account file not found");
-            }
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to initialize Firebase", e);
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return;
         }
+
+        FileInputStream serviceAccount =
+                new FileInputStream(firebasePath);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
     }
 }
